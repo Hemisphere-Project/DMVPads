3cecad9ff75d2894c708d661b304abc822e97a85
maigre <thomas@37m.gr>
2022-06-07 09:41:28 +0200

    base sketch

---
+++ DMV_Pads_Midi_Uno/DMV_Pads_Midi_Uno.ino
@@ -0,0 +1,60 @@
+// MID channel
+#define MIDI_CHANNEL 13
+
+// MIDI on SoftwareSerial
+//
+#include <MIDI.h>           // MIDI Library: https://github.com/FortySevenEffects/arduino_midi_library
+#include <SoftwareSerial.h>
+using Transport = MIDI_NAMESPACE::SerialMIDI<SoftwareSerial>;
+int rxPin = 8;
+int txPin = 9;
+SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
+Transport serialMIDI(mySerial);
+MIDI_NAMESPACE::MidiInterface<Transport> MIDI((Transport&)serialMIDI);
+
+// PADS
+//
+#include "VeloPad.h"  
+VeloPad* pads[8];
+
+// SETUP
+//
+void setup()
+{
+    // USB Serial (logs)
+    //
+    Serial.begin(115200);
+    delay(300);
+    Serial.println(".:: DMVPads ::.");
+
+    // Internal LED
+    //
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // Clean pad array
+    //
+    for (int i = 0; i < 8; i++) pads[i] = NULL;
+
+
+    // Init Pads: new VeloPad(analogPin, minTrig, maxTrig, midiNote/CC )
+    //
+    pads[0] = new VeloPad(A0, 200, 1024, 60 );   
+    pads[1] = new VeloPad(A1, 200, 1024, 61 );
+    pads[2] = new VeloPad(A2, 200, 1024, 62 );
+    pads[3] = new VeloPad(A3, 200, 1024, 63 );
+
+    // MIDI 
+    //
+    MIDI.begin(MIDI_CHANNEL); 
+    
+}
+
+// LOOP
+//
+void loop()
+{
+    // Update each pad
+    for (int i = 0; i < 8; i++) 
+      if (pads[i] != NULL) pads[i]->update();
+      
+}
---
+++ DMV_Pads_Midi_Uno/VeloPad.h
@@ -0,0 +1,84 @@
+class VeloPad 
+{       
+  public:
+    VeloPad(int _pin, int _minTrig, int _maxTrig, int _midiNote) {
+      pin = _pin;
+      minTrig = _minTrig;
+      maxTrig = _maxTrig;
+      midiNote = _midiNote;
+      
+      isOn = false;
+      counter = 0;    
+      currentVelocity = 0;
+    }
+
+    void update() {
+      int value = analogRead(pin);
+
+      // Not trigged
+      if ( value < minTrig ) 
+      {
+        currentVelocity = 0;
+        
+        // Was on -> send OFF
+        if ( isOn ) {
+           isOn = false;
+           MIDI.sendNoteOff(midiNote, 0, MIDI_CHANNEL);
+             Serial.print("[pad");
+             Serial.print(pin);
+             Serial.print("]: NOTEOFF ");
+             Serial.println(midiNote);
+        }
+      }
+
+      // Triggered
+      else {
+
+        // Map value to velocity
+        currentVelocity = map(value, minTrig, maxTrig, 0, 127);
+        if ( currentVelocity > 127 ) currentVelocity = 127;
+
+        // Was off -> send ON
+        if ( !isOn ) {
+           isOn = true;
+           MIDI.sendNoteOn(midiNote, currentVelocity, MIDI_CHANNEL);
+             Serial.print("[pad");
+             Serial.print(pin);
+             Serial.print("]: NOTEON ");
+             Serial.print(midiNote);
+             Serial.print(" @ ");
+             Serial.println(currentVelocity);
+        }
+        
+      }
+
+      // Send continuous CC value
+      MIDI.sendControlChange(midiNote, currentVelocity, MIDI_CHANNEL); 
+         Serial.print("[pad");
+         Serial.print(pin);
+         Serial.print("]: CC ");
+         Serial.print(midiNote);
+         Serial.print(" @ ");
+         Serial.print(currentVelocity);
+         Serial.print(" -- raw =  ");
+         Serial.println(value);
+    }
+
+  private:
+    int pin;
+    int minTrig;
+    int maxTrig;
+    int midiNote;
+    int currentVelocity;
+    bool isOn;
+    int counter;
+    
+};
+
+/*
+digitalWrite(LED_BUILTIN, HIGH);
+MIDI.sendNoteOn(42, 127, 1);    // Send a Note (pitch 42, velo 127 on channel 1)
+delay(1000);                    // Wait for a second
+MIDI.sendNoteOff(42, 0, 1);     // Stop the note
+digitalWrite(LED_BUILTIN, LOW);
+*/
